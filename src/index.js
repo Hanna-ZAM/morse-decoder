6efb71e8cc74371fd461cd609780335022fff056
Hanna Zalatarevich <a.zoloto@mail.ru>
2022-08-06 11:16:01 +0300

    init: start task morse-decoder

--- src/index.js
+++ src/index.js
@@ -35,10 +35,39 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+     '':' ',
 };
 
 function decode(expr) {
-    // write your solution here
+  let array = [];
+  let result ='';
+
+ for (let i=0; i<expr.length; i+=10){
+   array.push(expr.slice(i,(i+10)));  
+  }
+
+  array.forEach(element=>{
+
+    element=element.slice(element.indexOf('1'));
+    let n=element.length;
+    for (let j=0; j<element.length; j+=2) {
+      if ((element[j]+element[j+1])==='10') {
+        element=element+'.';
+              } else if ((element[j]+element[j+1])==='11') {
+        element=element+'-';
+      }}
+   element= element.slice(n);
+      
+    for (let key in MORSE_TABLE) {
+    if  (key===element){
+      result=result+MORSE_TABLE[key];
+         } 
+    }
+
+  })
+  
+  return result;
+     
 }
 
 module.exports = {
